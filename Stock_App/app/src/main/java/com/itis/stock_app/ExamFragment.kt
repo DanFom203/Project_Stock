cbba6dc10c42ffa5a9d20f9b0638dd862e3760d8
Saveliy Rozhnov <yanro1410@mail.ru>
2023-07-06 21:50:29 +0300

    sem1_ver1.1

--- Stock_App/app/src/main/java/com/itis/stock_app/ExamFragment.kt
+++ Stock_App/app/src/main/java/com/itis/stock_app/ExamFragment.kt
@@ -1,6 +1,27 @@
 package com.itis.stock_app
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.itis.stock_app.databinding.FragmentExamBinding
 
 class ExamFragment : Fragment(R.layout.fragment_exam) {
+    private var binding: FragmentExamBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentExamBinding.bind(view)
+
+        binding?.run {
+            btn2sem.setOnClickListener{
+                findNavController().navigate(R.id.action_examFragment_to_examSem1Fragment)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
